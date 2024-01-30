018e3d19e20c57573bfbc62a05333278cf08f8d5
Olan Gomez <johnolansaldivar.gomez@bicol-u.edu.ph>
2024-01-30 14:41:10 +0800

    feat: Added audio player button

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -9,7 +9,16 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(child: Text(nouns.first)),
+          child: Center(
+            child: TextButton(
+              onPressed: () async {
+                final player = AudioPlayer();
+                player.audioCache = AudioCache(prefix: 'assets/');
+                await player.play(AssetSource('note1.wav'));
+              },
+              child: Text('click me'),
+            ),
+          ),
         ),
       ),
     );
