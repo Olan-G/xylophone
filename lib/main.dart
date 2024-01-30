c0f3b75c3e8baa71a2b4d7273f6d4a323402c609
Olan Gomez <johnolansaldivar.gomez@bicol-u.edu.ph>
2024-01-30 15:39:46 +0800

    feat: Added all xylophone buttons

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,84 @@ import 'package:flutter/material.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int num) async {
+    final player = AudioPlayer();
+    player.audioCache = AudioCache(prefix: 'assets/');
+    await player.play(AssetSource('note$num.wav'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Center(
-            child: TextButton(
+            child: Column(
+          children: <Widget>[
+            TextButton(
               onPressed: () async {
-                final player = AudioPlayer();
-                player.audioCache = AudioCache(prefix: 'assets/');
-                await player.play(AssetSource('note1.wav'));
+                playSound(1);
               },
-              child: Text('click me'),
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.red)),
+              child: Container(),
             ),
-          ),
-        ),
+            TextButton(
+              onPressed: () async {
+                playSound(2);
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.orange)),
+              child: Container(),
+            ),
+            TextButton(
+              onPressed: () async {
+                playSound(3);
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.yellow)),
+              child: Container(),
+            ),
+            TextButton(
+              onPressed: () async {
+                playSound(4);
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green)),
+              child: Container(),
+            ),
+            TextButton(
+              onPressed: () async {
+                playSound(5);
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.blue)),
+              child: Container(),
+            ),
+            TextButton(
+              onPressed: () async {
+                playSound(6);
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.indigo)),
+              child: Container(),
+            ),
+            TextButton(
+              onPressed: () async {
+                playSound(7);
+              },
+              style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.purple)),
+              child: Container(),
+            ),
+          ],
+        )),
       ),
     );
   }
