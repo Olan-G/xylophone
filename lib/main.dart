3fb2251a66a2d011e2620509db11c429d593c75c
Olan Gomez <johnolansaldivar.gomez@bicol-u.edu.ph>
2024-01-30 20:34:38 +0800

    refactor: Fixed code redundancy
    Created functions to create the xylophone keys. Also fixed the style of the xylophone keys.

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,59 @@
+/*
+    John Olan S. Gomez
+    Software Engineer Intern
+    January 30, 2024
+    Using Flutter and Dart Packages to Speed Up Development
+    Xylophone App
+ */
+
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  // function to play sound
   void playSound(int num) async {
     final player = AudioPlayer();
     player.audioCache = AudioCache(prefix: 'assets/');
     await player.play(AssetSource('note$num.wav'));
   }
 
+  // function to build a key
+  Expanded buildKey({required int note, required Color color}) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () async {
+          playSound(note);
+        },
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(color),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(0))),
+          ),
+        ),
+        child: Container(),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
             child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            TextButton(
-              onPressed: () async {
-                playSound(1);
-              },
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.red)),
-              child: Container(),
-            ),
-            TextButton(
-              onPressed: () async {
-                playSound(2);
-              },
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.orange)),
-              child: Container(),
-            ),
-            TextButton(
-              onPressed: () async {
-                playSound(3);
-              },
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.yellow)),
-              child: Container(),
-            ),
-            TextButton(
-              onPressed: () async {
-                playSound(4);
-              },
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.green)),
-              child: Container(),
-            ),
-            TextButton(
-              onPressed: () async {
-                playSound(5);
-              },
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.blue)),
-              child: Container(),
-            ),
-            TextButton(
-              onPressed: () async {
-                playSound(6);
-              },
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.indigo)),
-              child: Container(),
-            ),
-            TextButton(
-              onPressed: () async {
-                playSound(7);
-              },
-              style: ButtonStyle(
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.purple)),
-              child: Container(),
-            ),
+            buildKey(color: Colors.red, note: 1),
+            buildKey(color: Colors.orange, note: 2),
+            buildKey(color: Colors.yellow, note: 3),
+            buildKey(color: Colors.green, note: 4),
+            buildKey(color: Colors.blue, note: 5),
+            buildKey(color: Colors.indigo, note: 6),
+            buildKey(color: Colors.purple, note: 7),
           ],
         )),
       ),
